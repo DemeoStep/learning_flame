c0cf41058f8a6cb0f1a4974a8c0c44f4836d1412
DemeoStep <demeo.alex@gmail.com>
2025-04-22 21:47:35 +0300

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:learning_flame/fly.dart';
@@ -10,25 +11,27 @@ import 'package:window_manager/window_manager.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
-    await windowManager.ensureInitialized();
+  if (!kIsWeb) {
+    if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+      await windowManager.ensureInitialized();
 
-    WindowOptions windowOptions = WindowOptions(
-      size: Size(600, 600),
-      center: true,
-      backgroundColor: Colors.transparent,
-      skipTaskbar: false,
-      titleBarStyle: TitleBarStyle.hidden,
-    );
+      WindowOptions windowOptions = WindowOptions(
+        size: Size(600, 600),
+        center: true,
+        backgroundColor: Colors.transparent,
+        skipTaskbar: false,
+        titleBarStyle: TitleBarStyle.hidden,
+      );
 
-    windowManager.waitUntilReadyToShow(windowOptions, () async {
-      await windowManager.show();
-      await windowManager.focus();
-    });
-  } else {
-    Flame.device.fullScreen();
-    Flame.device.setPortrait();
-    Flame.device.setOrientation(DeviceOrientation.portraitUp);
+      windowManager.waitUntilReadyToShow(windowOptions, () async {
+        await windowManager.show();
+        await windowManager.focus();
+      });
+    } else {
+      Flame.device.fullScreen();
+      Flame.device.setPortrait();
+      Flame.device.setOrientation(DeviceOrientation.portraitUp);
+    }
   }
 
   runApp(GameWidget(game: FlyGame()));
