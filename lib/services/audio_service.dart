c0b846f63a504f857adddce0b0ede7dd5f0b2506
DemeoStep <demeo.alex@gmail.com>
2025-05-13 12:37:55 +0300

    feat: simplify audio playback by removing audio pool management

--- lib/services/audio_service.dart
+++ lib/services/audio_service.dart
@@ -1,31 +1,14 @@
 import 'package:flame_audio/flame_audio.dart';
-import 'package:learning_flame/game/config.dart';
 import 'package:learning_flame/consts.dart';
 
 class AudioService {
   final double volume = 0.1;
-  final int _explosionsPoolSize = Config.maxAsteroidCount;
-  final int _cannonPoolSize = Config.maxClipSize;
-
-  AudioPool? _explosionPool;
-  AudioPool? _cannonPool;
-
-  Future<void> init() async {
-    _explosionPool ??= await FlameAudio.createPool(
-      Consts.explosion,
-      maxPlayers: _explosionsPoolSize,
-    );
-    _cannonPool ??= await FlameAudio.createPool(
-      Consts.gunFire,
-      maxPlayers: _cannonPoolSize,
-    );
-  }
 
   void playExplosion() {
-    _explosionPool?.start(volume: volume);
+    FlameAudio.play(Consts.explosion, volume: volume);
   }
 
   void playCannonFire() {
-    _cannonPool?.start(volume: volume);
+    FlameAudio.play(Consts.gunFire, volume: volume);
   }
 }
